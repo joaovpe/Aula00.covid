4a5e996b88ecb269f17ebdce8183c31e8ba7cc12
joaovpe <54962381+joaovpe@users.noreply.github.com>
2020-05-11 14:10:08 -0300

    Add files via upload

---
+++ Modulo B/aula5/Tarefa/javascript.js
@@ -0,0 +1,13 @@
+function medir()
+{
+   var resultado = document.getElementById("tempe").value;
+   if(resultado>35)
+    alert('[Muito quente');
+   else
+    alert('Muito frio'); 
+    if (resultado==35)
+    alert('Temperatura ideal');
+    else
+    alert('Não Está facil pra ninguem');
+    
+}
