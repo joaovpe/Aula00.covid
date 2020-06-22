694f3a875b27c3b3bb748ff22d9c53c87a6959d3
joaovpe <54962381+joaovpe@users.noreply.github.com>
2020-06-22 10:24:36 -0300

    Add files via upload

---
+++ Modulo F/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 9, 2, 3]
+/*adicionar um numero na posição desejada*/
+num[5]=10
+/*adicionar um numero em qualquer posição/*/
+num.push(7)
+/*Saber o tamanho do array/*/
+num.length
+/*Colocar os elementos em ordem crescente/*/
+num.sort()
+
+console.log('nosso vetor é' +num)
---
+++ Modulo F/vetornatela.js
@@ -0,0 +1,16 @@
+let valores = [8, 1, 7, 4, 2, 9]
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+console.log(valores[6])
+/*/
+
+for(let pos=0; pos < valores.length; pos++){
+    console.log("a posição "+pos" tem o valor de " +valores[pos])
+}
+/*/Encontrar o valor no vetor/ */
+let pos = num.indexOf()
